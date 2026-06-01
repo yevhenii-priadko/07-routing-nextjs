c7923b0e6bd6fa1759005d1ebc9725b3b8dfe2bb
Evgeniy-Sub8way <sub8way@gmail.com>
2026-06-01 19:11:16 +0300

    5

--- app/notes/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function NotesPage() {
-  // Автоматично перенаправляємо користувача на сторінку, де налаштований сайдбар
-  redirect('/notes/filter/all')
-}
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
 
-export default nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/notes',
+        destination: '/notes/filter/all',
+        permanent: true, // Сервер миттєво перенаправить на сторінку з працюючим сайдбаром
+      },
+    ]
+  },
+}
+
+export default nextConfig
